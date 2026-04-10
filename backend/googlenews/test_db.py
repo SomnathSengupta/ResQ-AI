b737954c7851b9cc8cf6de7e695b787c7f9c95bb
Arashikage21 <157001547+Arashikage21@users.noreply.github.com>
2026-04-10 07:37:01 +0530

    Delete backend/googlenews directory

--- backend/googlenews/test_db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-
-client = MongoClient("mongodb+srv://disaster_user:1234Arashikage@cluster0.zkggmy5.mongodb.net/?appName=Cluster0")
-
-print(client.list_database_names())
